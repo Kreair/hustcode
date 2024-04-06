a964de098ea03ce8afc4af20d4d16815404134f2
ferxas <ferxas321@hotmail.es>
2024-04-06 16:44:37 -0500

    deleting innecesary py file

--- src/switching_theme.py
+++
@@ -1,7 +0,0 @@
-def toggle_theme(self):
-    if self.light_mode:
-        self.setStyleSheet(open("./src/styles/style_dark.qss", "r").read())
-        self.light_mode = False
-    else:
-        self.setStyleSheet(open("./src/styles/style_light.qss", "r").read())
-        self.light_mode = True
